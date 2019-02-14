19216462a33f44028182f308f5c2a409cf5bacc8
Robert Kirsz <robert.kirsz@gmail.com>
2019-02-15 00:20:35 +0100

    Refactor

--- lejzi.src.js
+++ lejzi.src.js
@@ -1,69 +1,20 @@
-const isElementInViewport = element => {
-  const rect = element.getBoundingClientRect()
-
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+function lejzi(tag = 'lejzi', { root = null, rootMargin = '0px', threshold = 1 } = {}) {
+  const observer = new IntersectionObserver(
+    entries =>
+      entries
+        .filter(entry => entry.isIntersecting)
+        .forEach(({ target }) => {
+          observer.unobserve(target)
+          const image = new Image()
+          image.onload = () => {
+            if (target.tagName === 'IMG') target.src = target.dataset.lejzi
+            else target.style.backgroundImage = `url(${target.dataset.lejzi})`
+            target.removeAttribute(`data-${tag}`)
+          }
+          image.src = target.dataset.lejzi
+        }),
+    { root, rootMargin, threshold }
   )
-}
 
-const onVisibilityChange = (element, callback) => {
-  let oldVisible = false
-
-  return () => {
-    const visible = isElementInViewport(element)
-
-    if (visible !== oldVisible) {
-      oldVisible = visible
-
-      if (typeof callback == 'function') callback()
-    }
-  }
-}
-
-const lejzi = (className = '.lejzi') => {
-  if (!className) return
-
-  const elements = [...document.querySelectorAll(className)]
-
-  if (elements.length === 0) return
-
-  elements.forEach(element => {
-    const handler = onVisibilityChange(element, () => {
-      const image = new Image()
-
-      image.src = element.dataset.src
-
-      image.onload = () => {
-        element.style.backgroundImage = 'url(' + element.dataset.src + ')'
-        element.classList.remove(className.split('.').pop())
-
-        if (window.removeEventListener) {
-          removeEventListener('DOMContentLoaded', handler, false)
-          removeEventListener('load', handler, false)
-          removeEventListener('scroll', handler, false)
-          removeEventListener('resize', handler, false)
-        } else if (window.detachEvent) {
-          detachEvent('onDOMContentLoaded', handler) // IE9+ :(
-          detachEvent('onload', handler)
-          detachEvent('onscroll', handler)
-          detachEvent('onresize', handler)
-        }
-      }
-    })
-
-    if (window.addEventListener) {
-      addEventListener('DOMContentLoaded', handler, false)
-      addEventListener('load', handler, false)
-      addEventListener('scroll', handler, false)
-      addEventListener('resize', handler, false)
-    } else if (window.attachEvent) {
-      attachEvent('onDOMContentLoaded', handler) // IE9+ :(
-      attachEvent('onload', handler)
-      attachEvent('onscroll', handler)
-      attachEvent('onresize', handler)
-    }
-  })
+  Array.from(document.querySelectorAll(`[data-${tag}]`)).forEach(element => observer.observe(element))
 }
